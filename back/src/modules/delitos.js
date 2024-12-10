9543160c82788b1668308af9b35e4cd7182aa243
gooigi <lopez.gallego2611@gmail.com>
2024-12-10 08:11:28 -0500

    delitos crud

--- back/src/modules/delitos.js
+++ back/src/modules/delitos.js
@@ -52,23 +52,17 @@ delitos.get("/api/delitos/listartodos", (req, res) => {
 
 // editar ciudadano
 
-/* citizen.put("/api/citizen/editar/:id", (req, res) => {
+delitos.put("/api/delitos/editar/:id", (req, res) => {
   let id = req.params.id;
   let frmDatos = {
-    nombre_ciudadano: req.body.nombre,
-    apellido_ciudadano: req.body.apellido,
-    email_ciudadano: req.body.email,
-    apodo_ciudadano: req.body.apodo,
-    password_ciudadano: req.body.password,
-    fechaorigen: req.body.fecha,
-    especies_idespecie: req.body.especie,
-    roles_idrol: req.body.rol,
-    foto_ciudadano: req.body.foto,
+    nombre_delito: req.body.nombre,
+    descripcion_delito: req.body.descripcion,
+    grado_delitos_idgrado_delito: req.body.grado,
   };
 
-  let query = "UPDATE ciudadanos SET ? WHERE idciudadano = ?";
+  let query = "UPDATE delitos SET ? WHERE iddelito = ?";
 
-  bd.query(query, [frmDatos, id], (error, citizen) => {
+  bd.query(query, [frmDatos, id], (error, delitos) => {
     if (error) {
       res.send({
         status: "error",
@@ -79,18 +73,18 @@ delitos.get("/api/delitos/listartodos", (req, res) => {
       res.send({
         status: "ok",
         mensaje: "actualización exitosa",
-        citizen: citizen,
+        delitos: delitos,
       });
     }
   });
 });
 
-citizen.delete("/api/citizen/borrar/:id", (req, res) => {
+delitos.delete("/api/delitos/borrar/:id", (req, res) => {
   let id = req.params.id;
 
-  let consulta = "DELETE FROM ciudadanos WHERE idciudadano = ?";
+  let consulta = "DELETE FROM delitos WHERE iddelito = ?";
 
-  bd.query(consulta, [id], (error, citizen) => {
+  bd.query(consulta, [id], (error, delitos) => {
     if (error) {
       res.send({
         status: "error",
@@ -101,10 +95,10 @@ citizen.delete("/api/citizen/borrar/:id", (req, res) => {
       res.send({
         status: "ok",
         mensaje: "consulta exitosa",
-        citizen: citizen,
+        delitos: delitos,
       });
     }
   });
-}); */
+}); 
 
 module.exports = delitos;
