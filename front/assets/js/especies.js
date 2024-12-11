700f4292574f36378c17a7df1403edaa76c53969
lulo <lopez.gallego2611@gmail.com>
2024-12-10 19:22:50 -0500

    commit

--- front/assets/js/especies.js
+++ front/assets/js/especies.js
@@ -1,6 +1,15 @@
 let api = "https://interpolice-omfr.onrender.com/api/species/";
 let contenido = document.querySelector("#contenido");
 let btnNuevaEspecie = document.querySelector("#btnNuevaEspecie");
+let frmAction = "";
+
+const on = (element, event, selector, handler) => {
+  element.addEventListener(event, (e) => {
+    if (e.target.closest(selector)) {
+      handler(e);
+    }
+  });
+};
 
 //mostrar elementos en la tabla
 function listartodos() {
@@ -33,5 +42,81 @@ btnNuevaEspecie.addEventListener("click", () => {
   // limpiar los input
   nombre.value = "";
   frmAction = "crear";
+  frmCrearEspecie.show();
+});
+
+// boton submit
+frmSpecies.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // crear ciudadano
+  if (frmAction === "crear") {
+    fetch(api + "crear", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: nombre.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        alert("exito");
+        frmCrearEspecie.hide();
+        location.reload();
+      });
+  }
+
+  // editar ciudadano
+  if (frmAction === "editar") {
+    fetch(api + "editar", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+
+      body: JSON.stringify({
+        nombre: nombre.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.status, res.respuesta);
+        alert("exito");
+        frmCrearEspecie.hide();
+        location.reload();
+      });
+  }
+  frmCrearEspecie.hide();
+});
+
+on(document, "click", ".btnBorrar", (e) => {
+  let fila = e.target.parentNode.parentNode.parentNode;
+  let idform = fila.firstElementChild.innerText;
+  let respuesta = window.confirm(
+    `seguro que desea eliminar el registro con id: ${idform}`
+  );
+  console.log(idform);
+
+  if (respuesta) {
+    fetch(api + "borrar/" + idform, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        location.reload();
+      });
+  }
+});
+
+// llamar formulario de edición
+let idform = "";
+on(document, "click", ".btnEditar", (e) => {
+  let fila = e.target.parentNode.parentNode.parentNode;
+  console.log(fila);
+  let idciudadano = fila.children[0].innerText;
+  console.log(idform);
+  idform = idciudadano;
+  nombre.value = fila.children[1].innerText;
   frmCrearEspecie.show();
 });
